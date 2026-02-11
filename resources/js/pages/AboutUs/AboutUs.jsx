220849ff9b53fcb6bf798cf4394aa1a631752749
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-02-11 11:48:12 +0800

    added head title in about us page

--- resources/js/pages/AboutUs/AboutUs.jsx
+++ resources/js/pages/AboutUs/AboutUs.jsx
@@ -2,15 +2,19 @@ import TopSection from "@/components/components-about-us-page/top-section";
 import LeadersSection from "@/components/components-about-us-page/leaders-section";
 import GeoFootPrintSection from "@/components/components-about-us-page/geo-footprint-section";
 import QuoteSection from "@/components/components-about-us-page/quote-section";
+import { Head } from "@inertiajs/react";
 
 export default function AboutUs() {
  return (
-  <section className="max-w-7xl w-full mx-auto">
-   <TopSection />
-   <LeadersSection />
-   <div className="w-full bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] h-3 mt-10" />
-   <GeoFootPrintSection />
-   <QuoteSection />
-  </section>
+  <>
+   <Head title="About Us" />
+   <section className="max-w-7xl w-full mx-auto">
+    <TopSection />
+    <LeadersSection />
+    <div className="w-full bg-[radial-gradient(circle_at_center,#c89116,#c89116,#cfceaa)] h-3 mt-10" />
+    <GeoFootPrintSection />
+    <QuoteSection />
+   </section>
+  </>
  );
 }
