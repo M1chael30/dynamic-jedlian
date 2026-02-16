5cc0c55ba6e00ad36862ac67c1b2ea8c8250d7ce
JhibJhib <jhibrielbf2004@gmail.com>
2026-02-16 09:42:18 +0800

    created login page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Home\HomePageController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 //render home page
 Route::get('/', [HomePageController::class, 'index'])->name('home');
+Route::get('/login', [AuthController::class,'index'])->name('login');
 
 
 require __DIR__ . '/contactUs.php';
