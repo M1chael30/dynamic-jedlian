d8493f5fbd51561be8634d0431b17bec9c492779
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-03-05 11:50:08 +0800

    Update HomePageController.php

--- app/Http/Controllers/Home/HomePageController.php
+++ app/Http/Controllers/Home/HomePageController.php
@@ -11,8 +11,19 @@ class HomePageController extends Controller
 {
     public function index()
     {
-        $homestats = HomeStat::select("id","title","stat","description")->orderBy("id")->get();
-        $businesses = Business::select("id","name","description")->with(["business_images"])->orderBy("id")->get();
+        $homestats = HomeStat::select("id", "title", "stat", "description")
+            ->orderBy("id")
+            ->get();
+
+        $businesses = Business::select("id", "name", "description")
+            ->with([
+                "business_images" => fn($query) =>
+                $query->select('id', 'business_id', 'image_type', 'image_path')
+                    ->where('image_type', 'logo')
+            ])
+            ->orderBy("id")
+            ->get();
+
         return Inertia::render('Home', [
             'homestats' => $homestats,
             'businesses' => $businesses
