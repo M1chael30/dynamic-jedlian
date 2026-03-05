ccda5621b535bbcf26a3db319a49a6ef12436e5f
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-03-05 11:50:17 +0800

    Update sections-tab-card.jsx

--- resources/js/components/components-admin-business-management/tab-cards/sections-tab-card.jsx
+++ resources/js/components/components-admin-business-management/tab-cards/sections-tab-card.jsx
@@ -1,13 +1,11 @@
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Card, CardAction, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { TabsContent } from '@/components/ui/tabs';
-import { Item, ItemContent, ItemActions, ItemDescription } from '@/components/ui/item';
+import { Item, ItemContent, ItemActions, ItemDescription, ItemTitle } from '@/components/ui/item';
 
 
 import EditBusinessSectionForm from '../edit-business-section-form';
 import CreateBusinessSectionForm from '../create-business-section-form';
 import { Button } from '../../ui/button';
-import { TrashIcon } from 'lucide-react';
 import { router } from '@inertiajs/react';
 import { toast } from 'sonner';
 
@@ -38,7 +36,7 @@ export default function SectionsTabCard({ sections, business }) {
           {sections.map((section, index) => (
             <Item key={index} variant="outline" size="sm">
               <ItemContent>
-                <ItemDescription>{section?.title}</ItemDescription>
+                <ItemTitle>{section?.title}</ItemTitle>
                 <ItemDescription>{section?.content}</ItemDescription>
               </ItemContent>
               <ItemActions>
