fd186f856bb474dba5ea81ffe16e7f6febc3f75e
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-03-06 10:43:36 +0800

    Update AboutUsContentController.php

--- app/Http/Controllers/AboutUsContentController.php
+++ app/Http/Controllers/AboutUsContentController.php
@@ -8,16 +8,18 @@ use Inertia\Inertia;
 
 class AboutUsContentController extends Controller
 {
-    public function index(){
-        $content = AboutUsContent::select()->latest()->get();
+    public function index()
+    {
+        $content = AboutUsContent::select('id', 'title', 'content')->latest()->get();
         return Inertia::render('Admin/AboutUs/AboutUsManagement', [
             'content' => $content
         ]);
     }
 
-    public function update(Request $request, AboutUsContent $content){
+    public function update(Request $request, AboutUsContent $content)
+    {
         $updated = $request->validate([
-            'content'=> 'required|string|regex:/^\S{0,30}(\s+\S{1,30})*$/',
+            'content' => 'required|string|regex:/^\S{0,30}(\s+\S{1,30})*$/',
         ]);
 
         $content->update($updated);
