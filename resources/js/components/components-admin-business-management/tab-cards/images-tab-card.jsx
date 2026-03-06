ed14979a3110514f9a09a11c0f4858bdb9572a5f
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-03-06 09:20:57 +0800

    Update images-tab-card.jsx

--- resources/js/components/components-admin-business-management/tab-cards/images-tab-card.jsx
+++ resources/js/components/components-admin-business-management/tab-cards/images-tab-card.jsx
@@ -46,7 +46,7 @@ export default function ImagesTabsCard({ business, images }) {
                   <img
                     src={`/storage/${image.image_path}`}
                     alt="business image"
-                    className="h-full w-full object-center object-cover"
+                    className="h-full w-full object-center object-contain"
                   />
                 </div>
               </CardContent>
