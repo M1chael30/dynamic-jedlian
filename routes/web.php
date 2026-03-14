970f40cdf484835bd35aad7ffc5f6a4f60510ebe
Michael-Tejano <112233618+M1chael30@users.noreply.github.com>
2026-03-14 10:09:18 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,38 @@
 <?php
 
-use App\Http\Controllers\Home\HomePageController;
-use App\Http\Controllers\HomeManagementController;
-use App\Http\Middleware\EnsureUserIsAdminMiddleware;
+use App\Http\Controllers\Public\AboutUsController;
+use App\Http\Controllers\Public\AchievementController;
+use App\Http\Controllers\Public\BusinessController;
+use App\Http\Controllers\Public\ContactUsController;
+use App\Http\Controllers\Public\CorporateGovernanceController;
+use App\Http\Controllers\Public\HomePageController;
 use Illuminate\Support\Facades\Route;
 
-//render home page
-Route::get('/', [HomePageController::class, 'index'])->name('home');
 
+// public routes
+// home
+Route::get('/', [HomePageController::class, 'index'])
+    ->name('home');
 
+// about us
+Route::get('/about-us', [AboutUsController::class, 'index'])
+    ->name('about.us.index');
 
-Route::prefix('admin')
-    ->middleware(['auth', EnsureUserIsAdminMiddleware::class])
-    ->controller(HomeManagementController::class)
-    ->group(function () {
-        Route::get('/home-management', 'index')->name('home.management');
-        Route::put('/home-management/{homestat}/update', 'update')->name('update.homestat');
-    });
+// achievements
+Route::get('/achievements', [AchievementController::class, 'index'])
+    ->name('achievement.index');
 
-require __DIR__ . '/contactUs.php';
-require __DIR__ . '/achievements.php';
-require __DIR__ . '/corporateGovernance.php';
-require __DIR__ . '/aboutUs.php';
-require __DIR__ . '/users.php';
-require __DIR__ . '/profile.php';
+// businesses
+Route::get("/businesses/{business}", [BusinessController::class, 'index'])
+    ->name('business');
 
+// contact us
+Route::get('/contact-us', [ContactUsController::class, 'index'])
+    ->name('contact.us.index');
+
+// corporate governance
+Route::get('/corporate-governance', [CorporateGovernanceController::class, 'index'])
+    ->name('corporate.governance.index');
 
 require __DIR__ . '/auth.php';
 require __DIR__ . '/admin.php';
-require __DIR__ . '/businesses.php';
-
